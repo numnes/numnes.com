45dff7b8ccdeab297647de2a672ca71db522a453
matheusnunesismael <nunes.matheus.ismael@gmail.com>
2024-02-28 09:34:43 -0300

    feat(config.ts): atualiza informações do site, autor e descrição feat(config.ts): atualiza configurações de idioma para pt-br feat(config.ts): atualiza links das redes sociais feat(PostDetails.astro): adiciona classe de estilo para codeBlock feat(base.css): atualiza cores para o tema dark

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://numnes.com/", // replace this with your deployed domain
+  author: "Matheus Nunes Ismael",
+  desc: "Falando um pouco sobre arquitetura de software e afins.",
+  title: "Matheus Nunes Ismael",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
@@ -12,8 +12,8 @@ export const SITE: Site = {
 };
 
 export const LOCALE = {
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+  lang: "pt-br", // html lang code. Set this empty and default will be "en"
+  langTag: ["pt-BR"], // BCP 47 Language Tags. Set this empty [] to use the environment default
 } as const;
 
 export const LOGO_IMAGE = {
@@ -26,31 +26,31 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/numnes",
     linkTitle: ` ${SITE.title} on Github`,
     active: true,
   },
   {
     name: "Facebook",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com",
     linkTitle: `${SITE.title} on Facebook`,
-    active: true,
+    active: false,
   },
   {
     name: "Instagram",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://www.instagram.com/theusnunis/",
     linkTitle: `${SITE.title} on Instagram`,
     active: true,
   },
   {
     name: "LinkedIn",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://www.linkedin.com/in/matheus-nunes-ismael-82675914b/",
     linkTitle: `${SITE.title} on LinkedIn`,
     active: true,
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:numnes@proton.me",
     linkTitle: `Send an email to ${SITE.title}`,
     active: false,
   },
